638b9da6164a33042506c564393fbd855e2ad9ec
akhilTechHelps <132357452+akhilTechHelps@users.noreply.github.com>
2023-07-12 13:22:11 +0530

    Update UsersRoute.js

--- routes/UsersRoute.js
+++ routes/UsersRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router()
 import usersController from "../controller/UsersController.js";
-import uploader from "../middleware/uploader.js";
+
 
 router
 .post("/", usersController.handleSignup)
