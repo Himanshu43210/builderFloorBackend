a6f4622c179b2a8b85bf054d00604f60fefe2b90
akhilTechHelps <132357452+akhilTechHelps@users.noreply.github.com>
2023-07-12 22:26:01 +0530

    Update propertiesController.js

--- controller/propertiesController.js
+++ controller/propertiesController.js
@@ -4,11 +4,127 @@ import properties from '../models/propertiesModel.js';
 
 const getpropertiesList = async (req, res, next) => {
   try {
-    let data = await properties.find()
-    res.status(200).json({ data })
+    
+    let page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const {city} = req.query;
+
+    const queryObject = {};
+
+    if(city) {
+      queryObject.city = {$regex: city, $options: 'i'};
+    }
+    let skip = (page-1) *limit;
+
+    let data = await properties.find(queryObject).skip(skip).limit(limit)
+
+    let filteredProperties = data.data;
+    const totalDocuments = await properties.countDocuments();
+    const totalPages = Math.ceil(totalDocuments / limit);
+
+    res.status(200).json({ data,nbHits: data.length,pageNumber:page,totalPages: totalPages});
   } catch (error) {
-    res.status(400).json({ messgae: "An error Occoured" })
+    res.status(400).json({ messgae: error.message })
   }
+}
+
+const searchproperties = async (req, res, next) => {
+
+  const {city} = req.query;
+  const queryObject = {};
+
+  if(city) {
+    queryObject.city = {$regex: city, $options: 'i'};
+  }
+
+  const data = await properties.find(queryObject);
+
+  res.status(200).json({ data });
+}
+
+const filterproperties = async (req, res, next) => {
+
+  const filter = JSON.parse(req.query.filter);
+  console.log(filter);
+  
+  if (!filter) {
+    return res.status(400).json({ error: 'No filter provided' });
+  }
+
+  try {
+    
+    let filteredProperties = await properties.find();
+
+    if (filter.accommodation && filter.accommodation.length > 0) {
+      filteredProperties = filteredProperties.filter(property =>
+        filter.accommodation.includes(property.accommodation)
+      );
+    }
+
+    if (filter.categories && filter.categories.length > 0) {
+      filteredProperties = filteredProperties.filter(property =>
+        filter.categories.includes(property.category)
+      );
+    }
+
+    if (filter.cities && filter.cities.length > 0) {
+      filteredProperties = filteredProperties.filter(property =>
+        filter.cities.includes(property.city)
+      );
+    }
+
+    if (filter.facing && filter.facing.length > 0) {
+      filteredProperties = filteredProperties.filter(property =>
+        filter.facing.includes(property.facing)
+      );
+    }
+    //filter= {"accommodation":["3 BHK"],"categories":[],"cities":["KOLKATA","MUMBAI"],"facing":[],"floors":[],"locations":[],"possession":[],"possession":[],"priceRange":[],"sizeRange":[]}
+
+    if (filter.floors && filter.floors.length > 0) {
+      filteredProperties = filteredProperties.filter(property =>
+        filter.floors.includes(property.floor)
+      );
+    }
+
+    if (filter.possession && filter.possession.length > 0) {
+      filteredProperties = filteredProperties.filter(property =>
+        filter.possession.includes(property.possession)
+      );
+    }
+
+    if (filter.locations && filter.locations.length > 0) {
+      filteredProperties = filteredProperties.filter(property =>
+        filter.locations.includes(property.floor)
+      );
+    }
+
+    if (filter.priceRange && filter.priceRange.length === 2) {
+      const minPrice = filter.priceRange[0];
+      const maxPrice = filter.priceRange[1];
+      
+      filteredProperties = filteredProperties.filter(property =>
+        property.price >= minPrice && property.price <= maxPrice
+      );
+    }
+
+    if (filter.sizeRange && filter.sizeRange.length === 2) {
+      const minSize = filter.sizeRange[0];
+      const maxSize = filter.sizeRange[1];
+      
+      filteredProperties = filteredProperties.filter(property =>
+        property.size >= minSize && property.size <= maxSize
+      );
+    }
+
+
+
+
+    res.send(filteredProperties);
+
+  } catch (error) {
+    res.status(400).json({ messgae: error.message });
+  }
+  
 }
 
 const updatepropertiesByID = async (req, res, next) => {
@@ -107,5 +223,7 @@ export default {
   deletepropertiesById,
   updatepropertiesByID,
   updateBulkproperties,
-  insertBulkproperties
+  insertBulkproperties,
+  searchproperties,
+  filterproperties
 }
