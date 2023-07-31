ae32559fdd349ff198603ec495f079056ac342f4
akhilTechHelps <132357452+akhilTechHelps@users.noreply.github.com>
2023-07-31 21:58:23 +0530

    Update UsersRoute.js

--- routes/UsersRoute.js
+++ routes/UsersRoute.js
@@ -13,6 +13,6 @@ router
 .get("/filter", usersController.filterUsers)
 .post("/updateEdit", usersController.updateEditUsers)
 .delete("/deleteUser", usersController.deleteusersById)
-.post("/EditUpdate", usersController.Edit_update)
+.post("/EditUpdate", usersController.Edit_Update)
 
 export default router
