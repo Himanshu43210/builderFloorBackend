1398e2f3a20bbc90731be9908eb894569a805718
Himanshu Shukla <133512069+HimanshuTechHelps@users.noreply.github.com>
2023-08-07 19:38:02 +0530

    Update propertiesRoute.js

--- routes/propertiesRoute.js
+++ routes/propertiesRoute.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router
   .get("/getHomeData", propertiesController.getHomeData)
-  .get("/getSimilarProperties", propertiesController.getHomeData)
   .post("/searchPropertiesData", propertiesController.searchPropertiesData)
   .get("/list", propertiesController.getpropertiesList)
   .post("/addProperty", propertiesController.Edit_Update)
