4b2580eea22c5fd8ac259fbe20ae5ae17976486a
tanish-th <145182071+tanish-th@users.noreply.github.com>
2023-11-09 15:15:09 +0530

    Update formatters.js

--- utils/formatters.js
+++ utils/formatters.js
@@ -1,5 +1,3 @@
-const generatePropertyUrl = (property, baseUrl = 'https://builderfloor.com') => {
+export const generatePropertyUrl = (property, baseUrl = 'https://builderfloor.com') => {
     return baseUrl + `/${property.title?.replaceAll(" ", "-")}-${property.sectorNumber?.replaceAll(" ", "_")}-${property.size}SQYD-${property.floor?.replaceAll(" ", "_")}-${property.accommodation?.replaceAll(" ", "_")}-${property.facing}_FACING-${property.parkFacing === "YES" ? "park-" : ""}${property.corner === "YES" ? "corner-" : ""}${property.possession === "READY" ? "READY_POSSESSION" : "UNDER_CONSTRUCTION"}-${property._id}`;
 };
-
-module.exports = { generatePropertyUrl };
